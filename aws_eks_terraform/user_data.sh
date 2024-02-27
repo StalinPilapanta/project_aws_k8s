c80c4c5ed8052dc4702ba286d832a8abfde33692
Stalin <cpilapan@Stalins-MacBook-Pro.local>
2024-02-27 08:34:41 -0500

    Add git ignore

--- aws_eks_terraform/user_data.sh
+++ aws_eks_terraform/user_data.sh
@@ -26,6 +26,13 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 
+#install AWSCLI
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+#install kubectl 
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+sudo echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
---
+++ front-app-k8s/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React app deployed in Kubernetes deploy numero 1</h1>
+      <div className="App-link">
+          <p>PROTOTIPO DE PLANTILLA PARA LA AUTOMATIZACIÓN DE INFRAESTRUCTURA COMO CÓDIGO (IaC)</p>
+          <p>EN UN ENTORNO KUBERNETES SIGUIENDO PRÁCTICAS DE DEVOPS</p>
+        </div>
+        <div>
+          <img src='uisrael.png' alt=""></img>
+        </div>
+        <div className='form-box'>
+          <input type="text" name="movieName" placeholder='Name'/>
+          <input type="text" name="review" placeholder='Review'/>
+          <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
