c883dee5a51248adcd76aad3f0ab5d0dfdaf3600
StalinPilapanta <49129828+StalinPilapanta@users.noreply.github.com>
2024-03-11 19:58:50 -0500

    Update main.tf

--- aws_eks_terraform/main.tf
+++ aws_eks_terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-remote-state-projects"
+    bucket = "terraform-remote-state-projects-cspilapa"
     key    = "project/eks-aws"
     region = "us-east-1"
   }
