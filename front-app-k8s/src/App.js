7e998e5914af3eb2365d956d5f634a2c0d019e5f
StalinPilapanta <49129828+StalinPilapanta@users.noreply.github.com>
2024-03-13 14:17:29 -0500

    Update App.js

--- front-app-k8s/src/App.js
+++ front-app-k8s/src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React app deployed in Kubernetes deploy número 2 prueba online</h1>
+      <h1>React app deployed in Kubernetes deploy número 3 prueba online</h1>
       <div className="App-link">
           <p>PROTOTIPO DE PLANTILLA PARA LA AUTOMATIZACIÓN DE INFRAESTRUCTURA COMO CÓDIGO (IaC)</p>
           <p>EN UN ENTORNO KUBERNETES SIGUIENDO PRÁCTICAS DE DEVOPS</p>
         </div>
        <div className="App-link">
-          <p>Esta es una prueba de concepto</p>
+          <p>Esta es una prueba de concepto presentación componente practico</p>
         </div>
         <div>
           <img src='uisrael.png' alt=""></img>
